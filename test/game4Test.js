f8ef59cc73c54d27b19da4f1a3519a5cba2c1bd4
kaleabm18 <kaleabm18@gmail.com>
2025-06-04 12:45:17 +0300

    Solved: Game4 Test is passed

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,21 +1,31 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    const signer0 = ethers.provider.getSigner(0); // this will win
+    const signer1 = ethers.provider.getSigner(1); // this will write
+
+    const addr0 = await signer0.getAddress(); // winner
+    const addr1 = await signer1.getAddress(); // helper
+
+    return { game, signer0, signer1, addr0, addr1 };
   }
+
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer0, signer1, addr0 } = await loadFixture(deployContractAndSetVariables);
 
-    // nested mappings are rough :}
+    // signer1 writes to addr0 (winner)
+    await game.connect(signer1).write(addr0); // sets nested[addr0][signer1] = true
 
-    await game.win();
+    // signer0 (winner) now calls win(signer1)
+    await game.connect(signer0).win(await signer1.getAddress()); // checks nested[signer0][signer1]
 
-    // leave this assertion as-is
+    // Success condition
     assert(await game.isWon(), 'You did not win the game');
   });
 });
