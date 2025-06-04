fd2be794c96d75f31501956ad40f34db5b649008
kaleabm18 <kaleabm18@gmail.com>
2025-06-04 11:39:09 +0300

    solved Game3 Test

--- test/game3Test.js
+++ test/game3Test.js
@@ -1,55 +1,38 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game3', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game3');
     const game = await Game.deploy();
 
-    // Hardhat will create 10 accounts for you by default
-    // you can get one of this accounts with ethers.provider.getSigner
-    // and passing in the zero-based indexed of the signer you want:
-    const signer = ethers.provider.getSigner(0);
+    const signer0 = ethers.provider.getSigner(0); // low
+    const signer1 = ethers.provider.getSigner(1); // mid
+    const signer2 = ethers.provider.getSigner(2); // high
 
-    // you can get that signer's address via .getAddress()
-    // this variable is NOT used for Contract 3, just here as an example
-    const address = await signer.getAddress();
+    const addr0 = await signer0.getAddress();
+    const addr1 = await signer1.getAddress();
+    const addr2 = await signer2.getAddress();
 
-    return { game, signer };
+    return { game, signer0, signer1, signer2, addr0, addr1, addr2 };
   }
-  // this test is a bit different than the others
-  // it requires you to update the `balances` mapping in the Contract
-  // to win this stage
-  // you can do this by calling the `buy` function with a value of 1
-  // and then calling the `win` function with the correct arguments
-  // the `win` function expects three arguments:
-  // 1. the address of the signer
-  // 2. the value of the `balances` mapping for that signer
-  // 3. the signature of the signer
-  // you can get the signature by calling `signer.signMessage` with the address and value
-
-
-
-
-  // the `balances` mapping is a mapping of addresses to uint256 values
-  // you can update the `balances` mapping by calling the `buy` function
-  // with a value of 1
-  // this will update the `balances` mapping for the signer to 1
-  // then you can call the `win` function with the correct arguments
-  // the `win` function expects three arguments:
 
   it('should be a winner', async function () {
-    const { game, signer } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer0, signer1, signer2, addr0, addr1, addr2 } = await loadFixture(deployContractAndSetVariables);
 
-    // you'll need to update the `balances` mapping to win this stage
+    // LOW balance
+    await game.connect(signer0).buy({ value: ethers.utils.parseEther("1") });
 
-    // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: '1' });
-    const address = await signer.getAddress();
-    const balance = await game.balances(address); 
-    const signature = await signer.signMessage(ethers.utils.arrayify(balance));
-    await game.connect(signer).win(address, balance, signature);
-    // leave this assertion as-is
+    // MID balance
+    await game.connect(signer1).buy({ value: ethers.utils.parseEther("2") });
+
+    // HIGH balance
+    await game.connect(signer2).buy({ value: ethers.utils.parseEther("3") });
+
+    // Must be: win(mid, high, low)
+    await game.win(addr1, addr2, addr0);
+
     assert(await game.isWon(), 'You did not win the game');
   });
 });
