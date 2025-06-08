ec66b1c5f564b840c4cd3195e076df216a44483f
kaleabm18 <kaleabm18@gmail.com>
2025-06-08 20:08:01 +0300

    Added Payable, Approve

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,15 +2,26 @@
 pragma solidity ^0.8.17;
 
 contract Escrow {
-    
+
     address public arbiter;
     address public depositor;
     address public beneficiary;
+    bool public isApproved;
    
 
-    constructor(address _arbiter, address _beneficiary) {
+    constructor(address _arbiter, address _beneficiary) payable {
+        
         arbiter = _arbiter;
         beneficiary = _beneficiary;
         depositor = msg.sender;
     }
+
+    function approve() external 
+    {
+        require(msg.sender == arbiter);
+        (bool sent,) = beneficiary.call {value : address(this).balance} ("");
+        require(sent, "failed to send Ether");
+        isApproved=true;
+    }
+
 }
